685ff90b4f243580a85b0c85725ce179b0f3d381
serdg0 <sergio.diazg92@gmail.com>
2019-08-12 13:53:23 -0500

    added rubocop and tested with rubocop-a

--- lib/enumerables.rb
+++ lib/enumerables.rb
@@ -1,73 +1,73 @@
+# frozen_string_literal: true
+
 module Enumerable
+  def my_each
+    i = 0
+    while i < size
+      yield(self[i])
+      i += 1
+    end
+    self
+  end
 
-    def my_each
-        i = 0
-        while i < self.size
-            yield(self[i])
-            i += 1
-        end
-        self
+  def my_each_index
+    i = 0
+    while i < size
+      yield(self[i], i)
+      i += 1
     end
+  end
 
-    def my_each_index
-        i = 0
-        while i < self.size
-            yield(self[i],i)
-            i += 1
-        end
-    end
+  def my_select
+    arr_selection = []
+    my_each { |x| arr_selection << x if yield(x) }
+    arr_selection
+  end
 
-    def my_select
-        arr_selection = []
-        self.my_each{|x| arr_selection << x if yield(x)}
-        arr_selection
+  def my_all?
+    my_each do |x|
+      return true unless block_given?
+      return false unless yield(x)
     end
+    true
+  end
 
-    def my_all?
-        self.my_each do |x|
-        return true unless block_given?
-        return false unless yield(x)
-        end
-        true
+  def my_any?
+    statement = false
+    my_each do |x|
+      (statement = true) if yield(x)
     end
-    
-    def my_any?
-        statement = false
-        self.my_each do |x|
-            (statement = true) if yield(x)
-        end
-        statement
-    end
+    statement
+  end
 
-    def my_none?
-        statement = true
-        self.my_each{ |x| statement = false if yield(x) }
-        statement
-    end
+  def my_none?
+    statement = true
+    my_each { |x| statement = false if yield(x) }
+    statement
+  end
 
-    def my_count
-        count = 0
-        self.my_each do |x|
-            block_given? ? (count += 1 if yield(x)) : count += 1
-        end
-        count
+  def my_count
+    count = 0
+    my_each do |x|
+      block_given? ? (count += 1 if yield(x)) : count += 1
     end
+    count
+  end
 
-    def my_map
-        array = []
-        self.my_each do |x|
-            yield(x) ? (array << true) : (array << false)
-        end
-        array
+  def my_map
+    array = []
+    my_each do |x|
+      yield(x) ? (array << true) : (array << false)
     end
-        
-    def my_inject (memo = self[0])
-        object = memo
-        self.my_each do |x|
-            object = yield(memo,x)
-            memo = object 
-        end
-        object
+    array
+  end
+
+  def my_inject(memo = self[0])
+    object = memo
+    my_each do |x|
+      object = yield(memo, x)
+      memo = object
     end
-    
+    object
+  end
 end
--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,99 +1,93 @@
+# frozen_string_literal: true
+
 require './lib/enumerables'
 
 RSpec.describe Enumerable do
-
-    describe "#my_all?" do
-
-        it "returns true if all elements in the array are pairs" do
-            pairs = [2, 4, 6, 8, 10]
-            expect(pairs.my_all? { |x| x%2 == 0 }).to eql(true)
-        end
-
-        it "returns false is all elements in the array aren't pairs" do
-            odds = [1, 3, 5, 7]
-            expect(odds.my_all? { |x| x%2 == 0}).not_to eql(true)
-        end
-        
-        it "returns true for an empty array" do
-            expect([].my_all?).to eql(true)
-        end
+  describe '#my_all?' do
+    it 'returns true if all elements in the array are pairs' do
+      pairs = [2, 4, 6, 8, 10]
+      expect(pairs.my_all?(&:even?)).to eql(true)
     end
 
-    describe "#my_any?" do
-
-        it "returns true if an element from an array is pair" do
-            some = [1,2,5,7,9]
-            expect(some.my_any? { |x| x%2 == 0 }).to eql(true)
-        end
-
-        it "returns false if none of the elements in an array are pair" do
-            odds = [3,1,5,7]
-            expect(odds.my_any? { |x| x%2 == 0 }).not_to eql(true)
-        end
-
-        it "returns false if array is empty" do
-            expect([].my_any?).to eql(false)
-        end
+    it "returns false is all elements in the array aren't pairs" do
+      odds = [1, 3, 5, 7]
+      expect(odds.my_all?(&:even?)).not_to eql(true)
     end
 
-    describe "#my_count" do
+    it 'returns true for an empty array' do
+      expect([].my_all?).to eql(true)
+    end
+  end
 
-        it "returns the number of elements in an array" do
-            expect([1,2,3,4,5].my_count).to eql(5)
-        end
-
-        it "returns the number of pair elements in an array" do
-            expect([1,2,3,4].my_count { |x| x%2 == 0 }).to eql(2)
-        end
-
-        it "returns the number of words which length is equal to 3" do
-            expect(["do", "ten", "win", "great"].my_count { |word| word.length == 3}).to eql(2)
-        end
+  describe '#my_any?' do
+    it 'returns true if an element from an array is pair' do
+      some = [1, 2, 5, 7, 9]
+      expect(some.my_any?(&:even?)).to eql(true)
     end
 
-    describe "#my_none?" do
-
-        it "returns true if none of the elements in an array are pair" do
-            expect([1,3,5].my_none? { |x| x%2 == 0 }).to eql(true)
-        end
-
-        it "returns false if an element from an array is pair" do
-            expect([1,2,3,5].my_none? { |x| x%2 == 0 }).to eql(false)
-        end
-
-        it "returns true if an array is empty" do
-            expect([].my_none?).to eql(true)
-        end
+    it 'returns false if none of the elements in an array are pair' do
+      odds = [3, 1, 5, 7]
+      expect(odds.my_any?(&:even?)).not_to eql(true)
     end
 
-    describe "#my_select" do
+    it 'returns false if array is empty' do
+      expect([].my_any?).to eql(false)
+    end
+  end
 
-        it "returns an array with all the even elements" do
-            expect([1,2,4,5,7,9].my_select { |x| x%2 == 0 }).to eql([2,4])
-        end
-
-        it "returns an array with all the strings" do
-            expect(["a",1,"b",2,"c",3].my_select { |x| x.is_a? String }).to eql(["a", "b", "c"])
-        end
-
-        it "returns an array with all the integers" do
-            expect(["a",1,"b",2,"c",3].my_select { |x| x.is_a? Integer }).to eql([1, 2, 3])
-        end
+  describe '#my_count' do
+    it 'returns the number of elements in an array' do
+      expect([1, 2, 3, 4, 5].my_count).to eql(5)
     end
 
-    describe "#my_map" do
-
-        it "returns true for all even numbers and false otherwise" do
-            expect([2,1].my_map { |x| x%2 == 0 }).to eql([true, false])
-        end
-
-        it "returns true for every element that is a string" do
-            expect(["a",2,"c"].my_map { |x| x.is_a? String }).to eql([true, false, true])
-        end
-
-        it "returns true for each element that is not an integer" do
-            expect([true, 1, "a"].my_map { |x| x.is_a? Integer }).to eql([false, true, false])
-        end
+    it 'returns the number of pair elements in an array' do
+      expect([1, 2, 3, 4].my_count(&:even?)).to eql(2)
     end
 
+    it 'returns the number of words which length is equal to 3' do
+      expect(%w[do ten win great].my_count { |word| word.length == 3 }).to eql(2)
+    end
+  end
+
+  describe '#my_none?' do
+    it 'returns true if none of the elements in an array are pair' do
+      expect([1, 3, 5].my_none?(&:even?)).to eql(true)
+    end
+
+    it 'returns false if an element from an array is pair' do
+      expect([1, 2, 3, 5].my_none?(&:even?)).to eql(false)
+    end
+
+    it 'returns true if an array is empty' do
+      expect([].my_none?).to eql(true)
+    end
+  end
+
+  describe '#my_select' do
+    it 'returns an array with all the even elements' do
+      expect([1, 2, 4, 5, 7, 9].my_select(&:even?)).to eql([2, 4])
+    end
+
+    it 'returns an array with all the strings' do
+      expect(['a', 1, 'b', 2, 'c', 3].my_select { |x| x.is_a? String }).to eql(%w[a b c])
+    end
+
+    it 'returns an array with all the integers' do
+      expect(['a', 1, 'b', 2, 'c', 3].my_select { |x| x.is_a? Integer }).to eql([1, 2, 3])
+    end
+  end
+
+  describe '#my_map' do
+    it 'returns true for all even numbers and false otherwise' do
+      expect([2, 1].my_map(&:even?)).to eql([true, false])
+    end
+
+    it 'returns true for every element that is a string' do
+      expect(['a', 2, 'c'].my_map { |x| x.is_a? String }).to eql([true, false, true])
+    end
+
+    it 'returns true for each element that is not an integer' do
+      expect([true, 1, 'a'].my_map { |x| x.is_a? Integer }).to eql([false, true, false])
+    end
+  end
 end
